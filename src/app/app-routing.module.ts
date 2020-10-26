e19358843b058be3e02fe8913f80304ef5b9003b
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-26 15:37:14 -0600

    09 Protege rutas creando Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthGuard } from '@auth0/auth0-angular';
+// import { AuthGuard } from '@auth0/auth0-angular'; ng g guard services/AuthGuard
+// al generear guard no escribir guard para evitar repetir la palabra guard
 import { HomeComponent } from './components/home/home.component';
 import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuardGuard } from './services/auth-guard.guard';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'protegida',
     component: ProtegidaComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuardGuard]
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
