7c0550e8bc441a44eaae7f894cc9f0368b05d49d
Jose Luis <jluisvazquez.jv@gmail.com>
2020-10-26 13:46:44 -0600

    03 creacion de componentes y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProtegidaComponent
   ],
   imports: [
     BrowserModule,
